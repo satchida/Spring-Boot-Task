0e0cbff1a3d5ac27241e9eaf9e051e666e3d046d
satchida <you@example.com>
2019-08-07 13:03:39 +0530

    Global Exceptions

---
+++ src/main/java/com/stackroute/muzixapp/service/TrackService.java
@@ -0,0 +1,23 @@
+package com.stackroute.muzixapp.service;
+
+import com.stackroute.muzixapp.domain.Track;
+import com.stackroute.muzixapp.exceptions.TrackAlreadyExistsException;
+import com.stackroute.muzixapp.exceptions.TrackNotFoundException;
+
+import java.util.List;
+
+public interface TrackService  {
+
+
+    Track saveTrack(Track track) throws TrackAlreadyExistsException;
+
+    List<Track> getAllTracks();
+
+    boolean updateTrack(Track track, int id);
+
+    void deleteTrack(int id) throws TrackNotFoundException;
+
+    List<Track> findByTrackName(String name);
+
+    List<Track> getTrackByNameSortByName(String name);
+}
